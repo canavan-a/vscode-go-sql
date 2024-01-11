e0dcd94fae4d24c0ac60f1b49801eca08c5cdb38
canavan-a <aidan.canavan3@gmail.com>
2024-01-11 17:08:47 -0500

    union of ranges working

--- extension.js
+++ extension.js
@@ -6,7 +6,11 @@ const execSelectDecorationType = vscode.window.createTextEditorDecorationType({
 });
 
 const testDec = vscode.window.createTextEditorDecorationType({
-    color: '#DA70D6',
+    color: '#7FFF00',
+    isWholeLine: false
+});
+
+const removeDec = vscode.window.createTextEditorDecorationType({
     isWholeLine: false
 });
 
@@ -72,10 +76,35 @@ function doHighlight(matches) {
         while ((matchExecSelect = regexExecSelect.exec(text)) !== null) {
             const stringAfterExec = matchExecSelect[1];
 
+
+
             let firstIndex = matchExecSelect[0].indexOf("`")
             if (firstIndex == -1) {
                 firstIndex = matchExecSelect[0].indexOf(`"`)
             }
+
+
+            function doThing() {
+                let keyword = 'FROM'
+                const loc = findKeyword(stringAfterExec, keyword)
+
+                if (loc) {
+                    let sPos = document.positionAt(matchExecSelect.index + loc + firstIndex)
+                    let ePos = document.positionAt(matchExecSelect.index + loc + keyword.length + firstIndex)
+                    let kRange = new vscode.Range(sPos, ePos);
+                    const kDecoration = {
+                        range: kRange,
+                        hoverMessage: "sql keyword: " + keyword,
+                    }
+                    keywordSelected.push(kDecoration)
+                    return kRange
+                }
+                return null
+            }
+
+            const r1 = doThing()
+
+            console.log("hello")
 
 
 
@@ -85,30 +114,73 @@ function doHighlight(matches) {
 
             const range = new vscode.Range(startPos, endPos);
 
-            const decoration = {
-                range: range,
-                id: "x",
-                hoverMessage: 'SQL statement highlighted',
-            };
+            if (r1) {
+                console.log(range.start)
+                console.log("hello")
+                console.log(r1.start)
 
-            matchesExecSelect.push(decoration);
+
+                const newRanges = subtractRanges(range, r1)
+
+                for (let i = 0; i < newRanges.length; i++) {
+                    let dec = {
+                        range: newRanges[i]
+                    }
+                    matchesExecSelect.push(dec)
+                }
+
+            }
+            else {
+                const decoration = {
+                    range: range,
+                    // hoverMessage: 'SQL statement highlighted',
+                };
+
+                matchesExecSelect.push(decoration);
+            }
+
+
+            // console.log(newRanges)
+
+
         }
+
+
         editor.setDecorations(execSelectDecorationType, matchesExecSelect)
-        // const r = new vscode.Range(document.positionAt(0), document.positionAt(2000))
-        // editor.setDecorations(testDec, [{
-        //     range: r,
-        //     id: "x",
-        //     hoverMessage: 'SQL statement highlighted',
-        // },]);
+        editor.setDecorations(testDec, keywordSelected)
 
     }
 
 }
 
-function findKeyword(str, keyword) {
+function findKeyword(value, keyword) {
+    const v = value.indexOf(keyword)
+    if (v == -1) {
+        return null
+    }
+    return v
     // note: for keywords you will need to add  these styles first and then put the base over top.
 }
 
+function subtractRanges(originalRange, rangeToRemove) {
+    const resultRanges = [];
+
+    // Check if the ranges overlap
+    if (rangeToRemove.start.isAfterOrEqual(originalRange.start) && rangeToRemove.end.isBeforeOrEqual(originalRange.end)) {
+        // Split the original range into two parts, excluding the overlapping section
+        if (rangeToRemove.start.isAfter(originalRange.start)) {
+            resultRanges.push(new vscode.Range(originalRange.start, rangeToRemove.start));
+        }
+        if (rangeToRemove.end.isBefore(originalRange.end)) {
+            resultRanges.push(new vscode.Range(rangeToRemove.end, originalRange.end));
+        }
+    } else {
+        // No overlap, return the original range
+        resultRanges.push(originalRange);
+    }
+
+    return resultRanges;
+}
 
 module.exports = {
     activate,

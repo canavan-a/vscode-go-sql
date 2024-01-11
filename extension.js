630b7fdfc31692a29cd0acb73ee365c06871d6af
canavan-a <aidan.canavan3@gmail.com>
2024-01-11 11:39:10 -0500

    added support for multiple func calls, starting keyword implementation

--- extension.js
+++ extension.js
@@ -1,15 +1,12 @@
 const vscode = require('vscode');
 
-// Move the decoration type outside the activate function so that it is accessible globally
-const httpDecorationType = vscode.window.createTextEditorDecorationType({
-    // backgroundColor: 'red',
-    color: 'greenyellow',
-    isWholeLine: false  // Set this to false to only highlight the matched text
+const execSelectDecorationType = vscode.window.createTextEditorDecorationType({
+    color: '#FFA500',
+    isWholeLine: false
 });
 
-const execSelectDecorationType = vscode.window.createTextEditorDecorationType({
-    backgroundColor: 'lightblue',
-    color: 'green',
+const testDec = vscode.window.createTextEditorDecorationType({
+    color: '#DA70D6',
     isWholeLine: false
 });
 
@@ -40,54 +37,76 @@ function deactivate() { }
 
 function highlightExecSelect() {
     // Get the active text editor
-    const editor = vscode.window.activeTextEditor;
 
+    const editor = vscode.window.activeTextEditor;
     if (editor) {
+
         const document = editor.document;
 
         // Check if the document language is Go
         if (document.languageId === 'go') {
-            // Get the entire content of the editor
-            const text = document.getText();
-
-            // Define a regular expression to match ".Exec(" and the string that follows
-            const regexExecSelect = /\.Exec\((`[^`]*`|"(?:[^"\\]|\\.)*")/g;
-
-            // Find all matches in the text for ".Exec(" and the following string
-            let matchesExecSelect = [];
-            let matchExecSelect;
-            while ((matchExecSelect = regexExecSelect.exec(text)) !== null) {
-                // Extract the string content after .Exec(
-                const stringAfterExec = matchExecSelect[1];
-
-                // Calculate the start and end positions for the entire string after .Exec(
-                const startPos = document.positionAt(matchExecSelect.index + 7); // 7 is the length of ".Exec("
-                const endPos = document.positionAt(matchExecSelect.index + stringAfterExec.length + 5); // Adjust end position
-
-                // Create a range for the entire string after .Exec(
-                const range = new vscode.Range(startPos, endPos);
-
-                // Apply the light blue background color style to the entire string after .Exec(
-                const decoration = {
-                    range: range,
-                    hoverMessage: 'Highlighted string after .Exec(',
-                    backgroundColor: 'lightblue'
-                };
-
-                // Accumulate the decorations for the string after .Exec(
-                matchesExecSelect.push(decoration);
-            }
-
-            // Clear existing decorations before adding new ones
-            editor.setDecorations(execSelectDecorationType, []);
-
-            // Add all the decorations to the editor for the string after .Exec(
-            editor.setDecorations(execSelectDecorationType, matchesExecSelect);
+            doHighlight(["Exec", "QueryRow", "Query"])
         } else {
             // Clear decorations if the document is not a Go file
             editor.setDecorations(execSelectDecorationType, []);
         }
     }
+}
+
+function doHighlight(matches) {
+    const editor = vscode.window.activeTextEditor;
+    const document = editor.document;
+    const text = document.getText();
+
+    let matchesExecSelect = [];
+
+    let keywordSelected = []
+
+    for (let i = 0; i < matches.length; i++) {
+        const match = matches[i]
+
+        const regexExecSelect = new RegExp(`\\.${match}\\(\\s*(\`[^\\\`]*\`|"(?:[^"\\\\]|\\\\.)*")`, 'g');
+
+        let matchExecSelect;
+
+        while ((matchExecSelect = regexExecSelect.exec(text)) !== null) {
+            const stringAfterExec = matchExecSelect[1];
+
+            let firstIndex = matchExecSelect[0].indexOf("`")
+            if (firstIndex == -1) {
+                firstIndex = matchExecSelect[0].indexOf(`"`)
+            }
+
+
+
+            const startPos = document.positionAt(matchExecSelect.index + match.length + 3);
+            const endPos = document.positionAt(matchExecSelect.index + firstIndex + stringAfterExec.length - 1);
+
+
+            const range = new vscode.Range(startPos, endPos);
+
+            const decoration = {
+                range: range,
+                id: "x",
+                hoverMessage: 'SQL statement highlighted',
+            };
+
+            matchesExecSelect.push(decoration);
+        }
+        editor.setDecorations(execSelectDecorationType, matchesExecSelect)
+        // const r = new vscode.Range(document.positionAt(0), document.positionAt(2000))
+        // editor.setDecorations(testDec, [{
+        //     range: r,
+        //     id: "x",
+        //     hoverMessage: 'SQL statement highlighted',
+        // },]);
+
+    }
+
+}
+
+function findKeyword(str, keyword) {
+    // note: for keywords you will need to add  these styles first and then put the base over top.
 }
 
 
